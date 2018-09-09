f9c7fd92fff3862e3d97501019bb1d388ae6d343
mehdifallahi <mohammadf00900@gmail.com>
2018-09-09 18:29:32 +0430

    add item and marked and unmarked items

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NetworkService } from "./network/network.service";
 
 @Component({
-  selector: 'app-list-item',
-  templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.css']
+  selector: "app-list-item",
+  templateUrl: "./list-item.component.html",
+  styleUrls: ["./list-item.component.css"],
+  providers: [NetworkService]
 })
 export class ListItemComponent implements OnInit {
+  toDoList: any[];
 
-  constructor() { }
+  constructor(private networkServices: NetworkService) {}
 
   ngOnInit() {
+    this.toDoList = Object.values(this.networkServices.getToDoList());
+    this.toDoList.sort((a, b) => {
+      return a.isMarked - b.isMarked;
+    });
   }
 
+  addItem(title) {
+    this.networkServices.addItem(title.value);
+    title.value = null;
+  }
+
+  toggleMark(key: string, isMarked) {
+    this.networkServices.toggleMark(key, isMarked);
+  }
+
+  removeItem(key) {
+    this.networkServices.removeItem(key);
+  }
 }
--- src/app/list-item/network/network.service.ts
+++ src/app/list-item/network/network.service.ts
@@ -4,29 +4,33 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class NetworkService {
-  toDoList: {};
-  constructor(
-    private db: {
-      "1": { title: "go to gym"; isMarked: false };
-      "2": { title: "go to work"; isMarked: false };
-    }
-  ) {}
+  toDoList: { [key: string]: any };
+
+  constructor() {}
 
   getToDoList() {
-    this.toDoList = this.db;
+    this.toDoList = {
+      "1": { key: "1", title: "go to gym", isMarked: false },
+      "2": { key: "2", title: "go to work", isMarked: false }
+    };
     return this.toDoList;
   }
 
   addItem(title) {
     let db = Object.values(this.toDoList);
-    this.toDoList[db.length] = { title: title, isMarked: false };
+    this.toDoList[db.length] = {
+      key: db.length,
+      title: title,
+      isMarked: false
+    };
   }
 
-  toggleMark($key, flag) {
-    this.toDoList[$key].isMarked = flag;
+  toggleMark(key, flag) {
+    this.toDoList[key].isMarked = !flag;
   }
 
-  removeItem($key) {
-    delete this.toDoList[$key];
+  removeItem(key) {
+    delete this.toDoList[key];
+    console.log(this.toDoList);
   }
 }
